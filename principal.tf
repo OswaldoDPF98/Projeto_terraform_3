c2fbe7ef629e401895a4647cbec9e3e21b2c2ebb
OswaldoDPF98 <oswaldodpf98@gmail.com>
2025-07-02 18:30:25 -0300

    Criou-se um gateway de internet para a VPC

--- principal.tf
+++ principal.tf
@@ -1,20 +1,28 @@
 resource "aws_vpc" "padrao" {
-    cidr_block = "10.123.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support = true
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-    tags = {
-      Name = "VPC Padrao"
-    }
+  tags = {
+    Name = "VPC Padrao"
+  }
 }
 
 resource "aws_subnet" "subred_publica" {
-  vpc_id = aws_vpc.padrao.id
-    cidr_block = "10.123.1.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "sa-east-1a"
+  vpc_id                  = aws_vpc.padrao.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "sa-east-1a"
 
-    tags = {
-      Name = "Subred Publica"
-    }
+  tags = {
+    Name = "Subred Publica"
+  }
+}
+
+resource "aws_internet_gateway" "gateway_de_internet" {
+  vpc_id = aws_vpc.padrao.id
+
+  tags = {
+    Name = "Gateway de Internet"
+  }
 }
