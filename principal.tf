c8b01eb8ecf8937aa772b975ab271cd724fb033a
OswaldoDPF98 <oswaldodpf98@gmail.com>
2025-07-02 18:26:23 -0300

    Criou-se uma subred

--- principal.tf
+++ principal.tf
@@ -7,3 +7,14 @@ resource "aws_vpc" "padrao" {
       Name = "VPC Padrao"
     }
 }
+
+resource "aws_subnet" "subred_publica" {
+  vpc_id = aws_vpc.padrao.id
+    cidr_block = "10.123.1.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = "sa-east-1a"
+
+    tags = {
+      Name = "Subred Publica"
+    }
+}
