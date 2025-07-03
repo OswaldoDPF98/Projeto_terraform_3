921b565e3a2f3e3a6566ad784e9edc5c4c7cedff
OswaldoDPF98 <oswaldodpf98@gmail.com>
2025-07-02 22:21:28 -0300

    Consertou-se o arquivo 'outputs'

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "ip_publico_instancia" {
   description = "IP público da instância"
-  value       = aws_instance.instancia.public_ip
+  value       = aws_instance.nodo_desenvolvimento.public_ip
   
 }
